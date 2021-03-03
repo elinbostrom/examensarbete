b09a2a85b11e5b385a4b35bbc999c132e292783b
Elin Boström <ebostrom@live.se>
2021-03-03 14:11:39 +0100

    Change place of icon

--- components/BookingOverview/index.jsx
+++ components/BookingOverview/index.jsx
@@ -5,9 +5,9 @@ export default function BookingOverview() {
   return (
     <div className={styles.wrapper}>
       <h2>Bokningen är nu fulländad!</h2>
-      <img src="/icons/DressageIcon.svg" alt="häst ikon" />
       <p>Ett kvitto samt en bokningsbekräftelse har skickats till den angivna mejladressen.</p>
       <p>Vi ses i sadeln och du glöm inte hjälmen 😃</p>
+      <img src="/icons/DressageIcon.svg" alt="häst ikon" />
       <ButtonNavigate navigate="/rida/kurser" text="Våra kurser" />
     </div>
   );
