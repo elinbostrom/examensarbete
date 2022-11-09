b915d2440bc24e63273ba910fb78ced95db09b64
elinbostrom <ebostrom@live.se>
2022-11-09 16:48:47 +0100

    update graphql endpoint

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 // Components
-import Layout from '@/components/Layouts/index.jsx'
-import NewsStartSection from '@/components/NewsStartSection/index.jsx'
-import SectionStartRiding from '@/components/SectionStartRiding/index.jsx'
+import Layout from "@/components/Layouts/index.jsx";
+import NewsStartSection from "@/components/NewsStartSection/index.jsx";
+import SectionStartRiding from "@/components/SectionStartRiding/index.jsx";
 
 // get data
-import client from '../apollo/client'
-import { STARTPAGE } from '../queries/startpage';
+import client from "../apollo/client";
+import { STARTPAGE } from "../queries/startpage";
 
-export default function Home({ pageInfo, heroes, stablenews }) {
+export default function Home({ pageInfo, heroes, stablenews, animals }) {
   const { startRiding } = pageInfo[0].information.startpage;
   const { heroInfo } = heroes[0];
 
@@ -16,20 +16,19 @@ export default function Home({ pageInfo, heroes, stablenews }) {
       <NewsStartSection data={stablenews} />
       <SectionStartRiding data={startRiding} />
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps(context) {
-
   const { data, loading, networkStatus } = await client.query({
-    query: STARTPAGE
+    query: STARTPAGE,
   });
 
   return {
     props: {
       pageInfo: data?.newPages?.nodes,
       heroes: data?.heroes?.nodes,
-      stablenews: data?.stablenews?.nodes
+      stablenews: data?.stablenews?.nodes,
     },
-  }
+  };
 }
